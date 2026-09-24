77402324cf1dd43e20f34dbf9837778a68deaed3
Bukaj <jakub.wozny@interia.pl>
2026-09-24 17:40:11 +0100

    refactor: move tests to tests/ui directory

--- tests/UI/test_add_thing.py
+++ tests/UI/test_add_thing.py
@@ -1,9 +1,7 @@
-import time
 import pytest
 from selenium import webdriver
-from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.support.wait import WebDriverWait
-from webdriver_manager.firefox import GeckoDriverManager
+
 from pages.inventory_page import InventoryPage
 from pages.login_page import LoginPage
 from config import BASE_URL, GLOBAL_WAIT, USERS
@@ -16,17 +14,17 @@ class TestAddThing:
 
     @pytest.fixture(autouse=True)
     def setup_add_thing(self):
-        service = Service(GeckoDriverManager().install())
-        self.driver = webdriver.Firefox(service=service)
-        self.driver.maximize_window()
-        self.driver.wait = WebDriverWait(self.driver, GLOBAL_WAIT)
-        self.driver.get(BASE_URL)
+        self.driver =webdriver.Firefox
+        try:
+            self.driver.maximize_window()
+            self.driver.wait = WebDriverWait(self.driver, GLOBAL_WAIT)
+            self.driver.get(BASE_URL)
 
-        login_page = LoginPage(self.driver)
-        login_page.login(USERS["standard_user_login"]["user_name"], USERS["standard_user_login"]["user_password"])
-
-        yield
-        self.driver.quit()
+            login_page = LoginPage(self.driver)
+            login_page.login(USERS["standard_user_login"]["user_name"], USERS["standard_user_login"]["user_password"])
+            yield
+        finally:
+            self.driver.quit()
 
     def test_add_product_to_cart(self):
         inventory_page = InventoryPage(self.driver)
